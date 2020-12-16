15e5053177dfb518fb1b86d10e13c7e179ac61a7
Arun <iamavisnura@gmail.com>
2020-12-16 04:15:45 -0800

    pushing into dev ranch

--- src/gitproject1/Pusingcodeintogithub.java
+++ src/gitproject1/Pusingcodeintogithub.java
@@ -2,6 +2,10 @@ package gitproject1;
 
 public class Pusingcodeintogithub {
 
+	public void  changesdev()
+	{System.out.println("changes in dev");
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
